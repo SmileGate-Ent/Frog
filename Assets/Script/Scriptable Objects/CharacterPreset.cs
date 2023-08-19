13a9a310df7f0c85264636256d1d09f5fdadc2ba
Geoyeob Kim <gasbank@gmail.com>
2023-08-19 21:00:32 +0900

    캐릭터 프리셋 작업 중

--- Assets/Script/Scriptable Objects/CharacterPreset.cs
+++ Assets/Script/Scriptable Objects/CharacterPreset.cs
@@ -6,4 +6,8 @@ public class CharacterPreset : ScriptableObject
     [SerializeField] Sprite idleSprite;
     [SerializeField] Sprite attackSprite;
     [SerializeField] Sprite jumpSprite;
+
+    public Sprite IdleSprite => idleSprite;
+    public Sprite AttackSprite => attackSprite;
+    public Sprite JumpSprite => jumpSprite;
 }
