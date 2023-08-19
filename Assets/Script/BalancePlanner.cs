c095938cbc60454d38296d9242edd1808bb2f631
Geoyeob Kim <gasbank@gmail.com>
2023-08-20 00:45:26 +0900

    Spawner -> EnemySpawner / BalancePlanner wip

--- Assets/Script/BalancePlanner.cs
+++ Assets/Script/BalancePlanner.cs
@@ -2,5 +2,35 @@ using UnityEngine;
 
 public class BalancePlanner : MonoBehaviour
 {
+    public static BalancePlanner Instance;
+    
     [SerializeField] BalancePlan[] planList;
+
+    int curPlanIndex;
+    float gameTime;
+
+    public BalancePlan CurrentPlan => planList[curPlanIndex];
+    
+    BalancePlan NextPlan => curPlanIndex + 1 < planList.Length ? planList[curPlanIndex + 1] : null;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+        Debug.Log($"Current plan: {CurrentPlan}", CurrentPlan);
+    }
+
+    void Update()
+    {
+        gameTime += Time.deltaTime;
+
+        if (NextPlan != null && NextPlan.ApplyTime <= gameTime)
+        {
+            curPlanIndex++;
+            Debug.Log($"Current plan: {CurrentPlan}", CurrentPlan);
+        }
+    }
 }
--- Assets/Script/YB_Script/EnemySpawner.cs
+++ Assets/Script/YB_Script/EnemySpawner.cs
@@ -1,38 +1,41 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-public class Spawner : MonoBehaviour
+public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private GameObject monster;
     [SerializeField] private Vector2 moveTarget;
     [SerializeField] private LayerMask moveLayer;
     [SerializeField] private Transform[] camPos;
+
     private void Start()
     {
         NewTarget();
         StartCoroutine(SpawnCoroutine());
     }
+
     private void NewTarget()
     {
         if (Random.Range(0, 2) == 1)
         {
-            moveTarget = new Vector2(camPos[Random.Range(0,2)].position.x + Random.Range(-5f, 5f), Random.Range(-20f, 20f));
+            moveTarget = new Vector2(camPos[Random.Range(0, 2)].position.x + Random.Range(-5f, 5f),
+                Random.Range(-20f, 20f));
         }
         else
         {
-            moveTarget = new Vector2(Random.Range(-25f, 25f), camPos[Random.Range(2,4)].position.y + Random.Range(-5f, 5f));
+            moveTarget = new Vector2(Random.Range(-25f, 25f),
+                camPos[Random.Range(2, 4)].position.y + Random.Range(-5f, 5f));
         }
-        if (!Physics2D.OverlapCircle(moveTarget , 2, moveLayer)) NewTarget();
+
+        if (!Physics2D.OverlapCircle(moveTarget, 2, moveLayer)) NewTarget();
     }
+
     private IEnumerator SpawnCoroutine()
     {
-        Instantiate(monster,moveTarget, Quaternion.identity);
+        Instantiate(monster, moveTarget, Quaternion.identity);
         yield return new WaitForSeconds(Random.Range(5f, 10f));
         NewTarget();
         StartCoroutine(SpawnCoroutine());
     }
-
 }
