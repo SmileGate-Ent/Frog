63300364f02c0b9345e7ea3a2c8f8c9c8fd2f414
Pink_Poma <choi82354@gmail.com>
2023-08-19 21:12:38 +0900

    aaaa

--- Assets/Script/YB_Script/Spawner.cs
+++ Assets/Script/YB_Script/Spawner.cs
@@ -1,13 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Spawner : MonoBehaviour
 {
     [SerializeField] private GameObject monster;
     [SerializeField] private Vector2 moveTarget;
     [SerializeField] private LayerMask moveLayer;
-    
+    [SerializeField] private Transform[] camPos;
     private void Start()
     {
         NewTarget();
@@ -15,13 +17,21 @@ public class Spawner : MonoBehaviour
     }
     private void NewTarget()
     {
-        moveTarget = new Vector2(Random.Range(-33f, 33f), Random.Range(-23f, 23f));
+        if (Random.Range(0, 2) == 1)
+        {
+            moveTarget = new Vector2(camPos[Random.Range(0,2)].position.x + Random.Range(-5f, 5f), Random.Range(-20f, 20f));
+        }
+        else
+        {
+            moveTarget = new Vector2(Random.Range(-25f, 25f), camPos[Random.Range(2,4)].position.y + Random.Range(-5f, 5f));
+        }
         if (!Physics2D.OverlapCircle(moveTarget , 2, moveLayer)) NewTarget();
     }
     private IEnumerator SpawnCoroutine()
     {
         Instantiate(monster,moveTarget, Quaternion.identity);
         yield return new WaitForSeconds(Random.Range(5f, 10f));
+        NewTarget();
         StartCoroutine(SpawnCoroutine());
     }
 
